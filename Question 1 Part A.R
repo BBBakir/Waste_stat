b6c3982775a4a65d0c2b8fe5144049b93341e7ce
BBBakir <92781750+BBBakir@users.noreply.github.com>
2022-07-09 22:11:57 +0300

    Update Question 1 Part A.R

--- Question 1 Part A.R
+++ Question 1 Part A.R
@@ -8,11 +8,11 @@ library(reshape2)
 
 
 
-companyA <- read_excel("C:/GAMS/266/sw_data.xlsx", sheet = "Company A")
+companyA <- read_excel("sw_data.xlsx", sheet = "Company A")
 
 
 
-companyB <- read_excel("C:/GAMS/266/sw_data.xlsx", sheet = "Company B")
+companyB <- read_excel("sw_data.xlsx", sheet = "Company B")
 
 
 companyA$row_sum <- rowSums(companyA[,c(3:5)])
